edcf6290f11ad06f95cb2756405d33192c90b5f2
ViktorPakhai <ViktorPakhai@users.noreply.github.com>
2023-06-30 14:28:15 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ViktorPakhai"
+original_name="777"
+original_urlname="777"
+original_description="Awesome 777 created by ViktorPakhai"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv 777 $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from 777.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "777"
